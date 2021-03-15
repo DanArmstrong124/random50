ce2eee8db08e0ec9428e59a52eae430e40bba986
DanArmstrong124 <danielarmstrong124@gmail.com>
2021-03-15 16:34:38 +0000

    test

--- script.js
+++ script.js
@@ -49,6 +49,58 @@ var n48 = $(' #48 ');
 var n49 = $(' #49 ');
 var n50 = $(' #50 ');
 
+var w1 = "Name1";
+var w2 = "Name2";
+var w3 = "Name3";
+var w4 = "Name4";
+var w5 = "Name5";
+var w6 = "Name6";
+var w7 = "Name7";
+var w8 = "Name8";
+var w9 = "Name9";
+var w10 = "Name10";
+var w11 = "Name11";
+var w12 = "Name12";
+var w13 = "Name13";
+var w14 = "Name14";
+var w15 = "Name15";
+var w16 = "Name16";
+var w17 = "Name17";
+var w18 = "Name18";
+var w19 = "Name19";
+var w20 = "Name20";
+var w21 = "Name21";
+var w22 = "Name22";
+var w23 = "Name23";
+var w24 = "Name24";
+var w25 = "Name25";
+var w26 = "Name26";
+var w27 = "Name27";
+var w28 = "Name28";
+var w29 = "Name29";
+var w30 = "Name30";
+var w31 = "Name31";
+var w32 = "Name32";
+var w33 = "Name33";
+var w34 = "Name34";
+var w35 = "Name35";
+var w36 = "Name36";
+var w37 = "Name37";
+var w38 = "Name38";
+var w39 = "Name39";
+var w40 = "Name40";
+var w41 = "Name41";
+var w42 = "Name42";
+var w43 = "Name43";
+var w44 = "Name44";
+var w45 = "Name45";
+var w46 = "Name46";
+var w47 = "Name47";
+var w48 = "Name48";
+var w49 = "Name49";
+var w50 = "Name50";
+
+
 var winnum = $(' #winningnumber ');
 var winname = $(' #winningname ');
 
@@ -65,6 +117,24 @@ function getRandomInt(min, max) {
 button.click(function(){
     button.addClass("disabled");
     winningnum = getRandomInt(1, 51);
+    
+    setTimeout(function(){
+        if (winningnum === 1){
+            n1.css({"background-color": "green", "color": "white"});
+            winnum.css({"background-color": "green", "color": "white"});
+            winname.css({"background-color": "green", "color": "white"});
+            winnum.html(winningnum);
+            winname.html(w1);
+        }
+        else if (winningnum != 1){
+            n1.css({"background-color": "red", "color": "white"});
+            winnum.css({"background-color": "red", "color": "white"});
+            winname.css({"background-color": "red", "color": "white"});
+            winnum.html(winningnum);
+            winname.html(w1);
+
+        }
+    }, 1000);
 
 })
 
